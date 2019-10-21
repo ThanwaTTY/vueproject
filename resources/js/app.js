cc95f40257a5b63604ef9b017d383148e2d34d59
unknown <to.tanwa@gmail.com>
2019-10-21 18:49:31 +0700

    add vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
+
+import App from './components/index.vue'
+import router from './router/index.js'
+
+window.Vue = require('vue');
+
 require('./bootstrap');
+
+
+
+const app = new Vue({
+	el: '#app',
+	render: h => h(App),
+    router
+}); 
